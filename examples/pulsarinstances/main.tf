3b66e3040be132d285aa235d6ccb00787d53332c
Guangning E <guangning@streamnative.io>
2023-12-27 17:42:21 +0800

    Fixed main tf file

--- examples/pulsarinstances/main.tf
+++ examples/pulsarinstances/main.tf
@@ -8,15 +8,13 @@ terraform {
 }
 
 provider "streamnative" {
-  # Replace with your own client_id
-  client_id = "Uxfu8OTq3uwuGJeIliACwEkEmBxhdDH5"
   # Please replace path use your own key file path
   key_file_path = "/Users/tuteng/Downloads/sndev-terraform-ci-test.json"
 }
 
 resource "streamnative_pulsar_instance" "test-instance-1" {
   organization = "sndev"
-  name = "test-instance-1"
+  name = "test-instance-2"
   availability_mode = "zonal"
   pool_name = "shared-gcp"
   pool_namespace = "streamnative"
