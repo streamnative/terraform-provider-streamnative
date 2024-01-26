9ae1bcd5f8ff8b0080f03e4d9b68317ab6126c8e
Guangning E <guangning@streamnative.io>
2024-01-26 23:23:35 +0800

    Fixed test

--- cloud/pulsar_cluster_test.go
+++ cloud/pulsar_cluster_test.go
@@ -133,8 +133,8 @@ resource "streamnative_pulsar_cluster" "test-pulsar-cluster" {
 		  }
 		}
 		custom = {
-			allowAutoTopicCreation = "true"
-			bookkeeper.journalSyncData = "false"
+			"allowAutoTopicCreation" = "true"
+			"bookkeeper.journalSyncData" = "false"
 		}
 	}
 }
