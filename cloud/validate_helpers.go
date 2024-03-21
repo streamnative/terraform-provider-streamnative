5bf99ad612f3e8c37c2fd73ff95fa6fc86eafd78
mitch-hamm <150183009+mitch-hamm@users.noreply.github.com>
2024-03-21 09:47:45 -0500

    Move validation into the validate helpers (#33)

--- cloud/validate_helpers.go
+++ cloud/validate_helpers.go
@@ -62,3 +62,171 @@ func validateAuditLog(val interface{}, key string) (warns []string, errs []error
 	}
 	return
 }
+
+func validateCloudEnvionmentName(val interface{}, key string) (warns []string, errs []error) {
+	maxNameLength := 28
+	v := val.(string)
+	if len(v) > maxNameLength || len(v) < 1 {
+		errs = append(errs, fmt.Errorf(
+			"%q should be shorter than or equal to %d and greater than 0, got: %s", key, maxNameLength, v))
+	}
+	return
+}
+
+func validateRegion(val interface{}, key string) (warns []string, errs []error) {
+	v := val.(string)
+	if !contains(validRegions, v) {
+		errs = append(errs, fmt.Errorf(
+			"%q must be a valid region, got: %s", key, v))
+	}
+	return
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+var validRegions = []string{
+	//GCP
+	"us-west1",
+	"us-west2",
+	"us-west3",
+	"us-west4",
+	"us-central1",
+	"us-east1",
+	"us-east4",
+	"northamerica-northeast1",
+	"southamerica-east1",
+	"europe-west2",
+	"europe-west1",
+	"europe-west4",
+	"europe-west6",
+	"europe-west3",
+	"europe-north1",
+	"asia-south1",
+	"asia-southeast1",
+	"asia-southeast2",
+	"asia-east2",
+	"asia-east1",
+	"asia-northeast1",
+	"asia-northeast2",
+	"australia-southeast1",
+	"asia-northeast3",
+	//AWS
+	"us-east-2",
+	"us-east-1",
+	"us-west-1",
+	"us-west-2",
+	"af-south-1",
+	"ap-east-1",
+	"ap-south-2",
+	"ap-southeast-3",
+	"ap-southeast-4",
+	"ap-south-1",
+	"ap-northeast-3",
+	"ap-northeast-2",
+	"ap-southeast-1",
+	"ap-southeast-2",
+	"ap-northeast-1",
+	"ca-central-1",
+	"ca-west-1",
+	"eu-central-1",
+	"eu-west-1",
+	"eu-west-2",
+	"eu-south-1",
+	"eu-west-3",
+	"eu-south-2",
+	"eu-north-1",
+	"eu-central-2",
+	"il-central-1",
+	"me-south-1",
+	"me-central-1",
+	"sa-east-",
+	"eastus",
+	//Azure
+	"eastus2",
+	"southcentralus",
+	"westus2",
+	"westus3",
+	"australiaeast",
+	"southeastasia",
+	"northeurope",
+	"swedencentral",
+	"uksouth",
+	"westeurope",
+	"centralus",
+	"southafricanorth",
+	"centralindia",
+	"eastasia",
+	"japaneast",
+	"koreacentral",
+	"canadacentral",
+	"francecentral",
+	"germanywestcentral",
+	"norwayeast",
+	"polandcentral",
+	"switzerlandnorth",
+	"uaenorth",
+	"brazilsouth",
+	"centraluseuap",
+	"qatarcentral",
+	"centralusstage",
+	"eastusstage",
+	"eastus2stage",
+	"northcentralusstage",
+	"southcentralusstage",
+	"westusstage",
+	"westus2stage",
+	"asia",
+	"asiapacific",
+	"australia",
+	"brazil",
+	"canada",
+	"europe",
+	"france",
+	"germany",
+	"global",
+	"india",
+	"japan",
+	"korea",
+	"norway",
+	"singapore",
+	"southafrica",
+	"switzerland",
+	"uae",
+	"uk",
+	"unitedstates",
+	"unitedstateseuap",
+	"eastasiastage",
+	"southeastasiastage",
+	"brazilus",
+	"eastusstg",
+	"northcentralus",
+	"westus",
+	"jioindiawest",
+	"eastus2euap",
+	"southcentralusstg",
+	"westcentralus",
+	"southafricawest",
+	"australiacentral",
+	"australiacentral2",
+	"australiasoutheast",
+	"japanwest",
+	"jioindiacentral",
+	"koreasouth",
+	"southindia",
+	"westindia",
+	"canadaeast",
+	"francesouth",
+	"germanynorth",
+	"norwaywest",
+	"switzerlandwest",
+	"ukwest",
+	"uaecentral",
+	"brazilsoutheas",
+}
