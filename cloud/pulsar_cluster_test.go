8a6390b62358266bd5e3cb7f96e7218b6413b9a2
mitch-hamm <mitch.hamm@streamnative.io>
2024-04-03 15:49:07 -0500

    Update pulsar_cluster_test.go

--- cloud/pulsar_cluster_test.go
+++ cloud/pulsar_cluster_test.go
@@ -20,6 +20,7 @@ import (
 	"math/rand"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
@@ -52,7 +53,7 @@ func TestPulsarCluster(t *testing.T) {
 }
 
 func testCheckPulsarClusterDestroy(s *terraform.State) error {
-	//time.Sleep(10 * time.Second)
+	time.Sleep(10 * time.Second)
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "streamnative_pulsar_cluster" {
 			continue
