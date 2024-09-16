4ce03e7001c197cb8714bbf4a08db807ad3246cc
mitch-hamm <mitch.hamm@streamnative.io>
2024-09-16 11:01:43 -0700

    Update apikey_test.go

--- cloud/apikey_test.go
+++ cloud/apikey_test.go
@@ -28,9 +28,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var apiKeyGeneratedName = fmt.Sprintf("terraform-test-api-key-%d", rand.Intn(10000))
-
 func TestApiKey(t *testing.T) {
+	var apiKeyGeneratedName = fmt.Sprintf("terraform-test-api-key-%d", rand.Intn(10000))
 	resource.Test(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: testAccProviderFactories,
