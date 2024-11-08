19f4ea8a7c7ecef2fb3e978e023d2bab88739c88
mitch-hamm <mitch.hamm@streamnative.io>
2024-11-08 14:12:22 -0800

    Update test for 10 char limit

--- cloud/apikey_test.go
+++ cloud/apikey_test.go
@@ -28,7 +28,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var apiKeyGeneratedName = fmt.Sprintf("terraform-test-api-key-%d", rand.Intn(10000))
+var apiKeyGeneratedName = fmt.Sprintf("e2e-%d", rand.Intn(10000))
 
 func TestApiKey(t *testing.T) {
 	resource.Test(t, resource.TestCase{
