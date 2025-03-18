8614ecf2b8ac588952de038f351ba35b2a92c0ea
mitch-hamm <150183009+mitch-hamm@users.noreply.github.com>
2025-03-18 13:25:12 -0500

    Marks fields sensitive and use ForceNew (#106)
    Hide sensitive fields
    Use Force New to allow users to replace a resource rather than erroring
    Add instance_name and token as readable values to match data source
    behaviour

--- cloud/resource_apikey.go
+++ cloud/resource_apikey.go
@@ -45,15 +45,6 @@ func resourceApiKey() *schema.Resource {
 				// This is create event, so we don't need to check the diff.
 				return nil
 			}
-			if diff.HasChange("name") ||
-				diff.HasChange("organization") ||
-				diff.HasChange("instance_name") ||
-				diff.HasChange("service_account_name") ||
-				diff.HasChange("expiration_time") {
-				return fmt.Errorf("ERROR_UPDATE_API_KEY: " +
-					"The api key does not support updates organization, " +
-					"name, instance_name, service_account_name and expiration_time, please recreate it")
-			}
 			return nil
 		},
 		Importer: &schema.ResourceImporter{
@@ -76,28 +67,39 @@ func resourceApiKey() *schema.Resource {
 			"organization": {
 				Type:         schema.TypeString,
 				Required:     true,
+				ForceNew:     true,
 				Description:  descriptions["organization"],
 				ValidateFunc: validateNotBlank,
 			},
 			"name": {
 				Type:         schema.TypeString,
 				Required:     true,
+				ForceNew:     true,
 				Description:  descriptions["apikey_name"],
 				ValidateFunc: validateNotBlank,
 			},
 			"instance_name": {
 				Type:        schema.TypeString,
 				Required:    true,
+				ForceNew:    true,
 				Description: descriptions["instance_name"],
+			},
+			"token": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Sensitive:   true,
+				Description: descriptions["token"],
 			},
 			"service_account_name": {
 				Type:        schema.TypeString,
 				Required:    true,
+				ForceNew:    true,
 				Description: descriptions["service_account_name"],
 			},
 			"expiration_time": {
 				Type:        schema.TypeString,
 				Optional:    true,
+				ForceNew:    true,
 				Description: descriptions["expiration_time"],
 			},
 			"revoke": {
@@ -128,6 +130,7 @@ func resourceApiKey() *schema.Resource {
 			"private_key": {
 				Type:        schema.TypeString,
 				Computed:    true,
+				Sensitive:   true,
 				Description: descriptions["private_key"],
 			},
 			"key_id": {
