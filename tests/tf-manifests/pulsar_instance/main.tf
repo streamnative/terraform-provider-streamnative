f14f731f65bb4082e743f0f7189cb80a93e9e00d
mitch-hamm <mitch.hamm@streamnative.io>
2025-03-21 15:35:25 -0700

    update

--- tests/tf-manifests/pulsar_instance/main.tf
+++ tests/tf-manifests/pulsar_instance/main.tf
@@ -11,13 +11,23 @@ provider "streamnative" {
   key_file_path = pathexpand("~/service_account.json")
 }
 
+locals {
+  rand = replace(substr(timestamp(), 11, 8), ":", "")
+}
+
 resource "streamnative_pulsar_instance" "test-instance" {
   organization = "sndev"
-  name = "terraform-pulsar-instance-test"
+  name = "terraform-pulsar-instance-test-${local.rand}"
   availability_mode = "zonal"
   pool_name = "shared-gcp"
   pool_namespace = "streamnative"
   type = "standard"
+
+  lifecycle {
+    ignore_changes = [
+      name,
+    ]
+  }
 }
 
 data "streamnative_pulsar_instance" "test-instance" {
