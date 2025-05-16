46fc6076aa5b3cc22bb911ff43f36250366c541c
Guangning E <guangning@streamnative.io>
2025-05-16 11:45:08 +0800

    Fixed location check

--- cloud/resource_pulsar_cluster.go
+++ cloud/resource_pulsar_cluster.go
@@ -470,9 +470,21 @@ func resourcePulsarClusterCreate(ctx context.Context, d *schema.ResourceData, me
 		}
 	}
 	if pulsarInstance.IsServerless() {
-		if location != "us-east-2" && location != "us-central1" && location != "eastus" {
+		if pulsarInstance.Spec.PoolRef.Namespace != "streamnative" {
 			return diag.FromErr(fmt.Errorf("ERROR_CREATE_PULSAR_CLUSTER: " +
-				"location must be us-east-2 for aws, us-central1 for gcp or eastus for azure for serverless instance"))
+				"pool member must be in streamnative namespace for serverless instance"))
+		}
+		if pulsarInstance.Spec.PoolRef.Name == "shared-aws" && location != "us-east-2" {
+			return diag.FromErr(fmt.Errorf("ERROR_CREATE_PULSAR_CLUSTER: " +
+				"location must be us-east-2 for aws serverless instance"))
+		}
+		if pulsarInstance.Spec.PoolRef.Name == "shared-gcp" && location != "us-central1" {
+			return diag.FromErr(fmt.Errorf("ERROR_CREATE_PULSAR_CLUSTER: " +
+				"location must be us-central1 for gcp serverless instance"))
+		}
+		if pulsarInstance.Spec.PoolRef.Name == "shared-azure" && location != "eastus" {
+			return diag.FromErr(fmt.Errorf("ERROR_CREATE_PULSAR_CLUSTER: " +
+				"location must be eastus for azure serverless instance"))
 		}
 	}
 	if !ursaEnabled && !pulsarInstance.IsServerless() {
